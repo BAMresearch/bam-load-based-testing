aaa4d175af6656cc245f4631862133ffdbd643ac
Stephan Göbel <stephan.goebel@rwth-aachen.de>
2025-05-16 10:21:05 +0200

    changed used example building

--- Example/modelOnTestBench.py
+++ Example/modelOnTestBench.py
@@ -5,7 +5,7 @@ import time
 stepSize = 1  #ToDo: Allign step size with your own step size
 
 # Create Reduced building
-BamBuilding = BAM_RRT_HP3_variableflow_8Kcalc.MTBui_A
+BamBuilding = BAM_RRT_HP3_variableflow_8Kcalc.MTBui_F
 
 while True:
     t1 = time.time()
