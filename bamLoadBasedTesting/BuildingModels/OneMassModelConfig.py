e5da03772c5a925181bae9ae8b78207a8aaf6d79
Jonas Klauke <jonas.klauke@eonerc.rwth-aachen.de>
2026-06-08 15:37:29 +0200

    Added thermal design capacity

--- bamLoadBasedTesting/BuildingModels/OneMassModelConfig.py
+++ bamLoadBasedTesting/BuildingModels/OneMassModelConfig.py
@@ -9,11 +9,10 @@ t_b = 20                    # in °C; see indoor temperature in EN 14825; consta
 t_flow_design = 55          # in °C; design supply temperature
 
 deltaT = 8                  # in K; temperature difference in condenser
-# TODO use mass flow rate from standard condition (P_rated)?
-mass_flow_design = q_design_e / (4183*deltaT)   # in kg/s; design mass flow of heating system
+mass_flow_design = q_design_e / (4183*deltaT)   # in kg/s; declared mass flow rate from manufacturer
 
 # Parameters for one mass model
-tau_h = 2384                # in s; time constant of mass, TODO
+c_design = 67               # in J/K/W_design
 use_dynamic_load = True     # if True use dynamic load, if False use fixed load
 use_constant_mflow = True   # if True use constant flow (variable dT), if False use variable flow (constant dT)
 
@@ -22,31 +21,31 @@ t_ret_design = t_flow_design - deltaT
 dt_mean_design = (t_flow_design - t_ret_design) / math.log((t_b - t_flow_design) / (t_b - t_ret_design))
 
 # Design without hydraulic switch and virtual BUH
-ParaMTBui_E = CalcParameters(t_a_design=t_a_design, t_a=-10, relHum=69.38, q_design=q_design_e, PLC=1, tau_h=tau_h,
+ParaMTBui_E = CalcParameters(t_a_design=t_a_design, t_a=-10, relHum=69.38, q_design=q_design_e, PLC=1, c_design=c_design,
                              t_flow_design=t_flow_design, t_flow_plc=55, t_b=t_b, m_dot_H_design=mass_flow_design,
                              constant_mflow=use_constant_mflow, dt_mean=dt_mean_design, dt_mean_design=dt_mean_design,
                              delta_T_cond_design=deltaT, q_def_corr=0)
 MTBui_E = ParaMTBui_E.createBuilding(dynamic_load=use_dynamic_load)
 
-ParaMTBui_A = CalcParameters(t_a_design=t_a_design, t_a=-7, relHum=74.69, q_design=q_design_e, PLC=0.8846,  tau_h=tau_h,
+ParaMTBui_A = CalcParameters(t_a_design=t_a_design, t_a=-7, relHum=74.69, q_design=q_design_e, PLC=0.8846, c_design=c_design,
                              t_flow_design=t_flow_design, t_flow_plc=52, t_b=t_b, m_dot_H_design=mass_flow_design,
                              constant_mflow=use_constant_mflow, dt_mean=48.3-t_b, dt_mean_design=dt_mean_design,
                              delta_T_cond_design=deltaT)
 MTBui_A = ParaMTBui_A.createBuilding(dynamic_load=use_dynamic_load)
 
-ParaMTBui_B = CalcParameters(t_a_design=t_a_design, t_a=2, relHum=83.87, q_design=q_design_e, PLC=0.5385, tau_h=tau_h,
+ParaMTBui_B = CalcParameters(t_a_design=t_a_design, t_a=2, relHum=83.87, q_design=q_design_e, PLC=0.5385, c_design=c_design,
                              t_flow_design=t_flow_design, t_flow_plc=42, t_b=t_b, m_dot_H_design=mass_flow_design,
                              constant_mflow=use_constant_mflow, dt_mean=39.8-t_b, dt_mean_design=dt_mean_design,
                              delta_T_cond_design=deltaT)
 MTBui_B = ParaMTBui_B.createBuilding(dynamic_load=use_dynamic_load)
 
-ParaMTBui_C = CalcParameters(t_a_design=t_a_design, t_a=7, relHum=86.84, q_design=q_design_e, PLC=0.3462, tau_h=tau_h,
+ParaMTBui_C = CalcParameters(t_a_design=t_a_design, t_a=7, relHum=86.84, q_design=q_design_e, PLC=0.3462, c_design=c_design,
                              t_flow_design=t_flow_design, t_flow_plc=36, t_b=t_b, m_dot_H_design=mass_flow_design,
                              constant_mflow=use_constant_mflow, dt_mean=34.6-t_b, dt_mean_design=dt_mean_design,
                              delta_T_cond_design=deltaT)
 MTBui_C = ParaMTBui_C.createBuilding(dynamic_load=use_dynamic_load)
 
-ParaMTBui_D = CalcParameters(t_a_design=t_a_design, t_a=12, relHum=88.94, q_design=q_design_e, PLC=0.1538, tau_h=tau_h,
+ParaMTBui_D = CalcParameters(t_a_design=t_a_design, t_a=12, relHum=88.94, q_design=q_design_e, PLC=0.1538, c_design=c_design,
                              t_flow_design=t_flow_design, t_flow_plc=30, t_b=t_b, m_dot_H_design=mass_flow_design,
                              constant_mflow=use_constant_mflow, dt_mean=29.4-t_b, dt_mean_design=dt_mean_design,
                              delta_T_cond_design=deltaT)
--- bamLoadBasedTesting/oneMassModel.py
+++ bamLoadBasedTesting/oneMassModel.py
@@ -197,7 +197,7 @@ class OneMassBuilding:
 
 class CalcParameters:
     def __init__(self, t_a_design, t_a, q_design, PLC, t_flow_design, t_flow_plc, m_dot_H_design, constant_mflow,
-                 delta_T_cond_design, dt_mean=0, dt_mean_design=0, tau_h=505E3/258, t_b=20, boostHeat = False,
+                 delta_T_cond_design, c_design, dt_mean=0, dt_mean_design=0, t_b=20, boostHeat = False,
                  maxPowBooHea = 0, hydraulicSwitch = False, relHum = 0, q_def_corr=0):
         """
         Calculate parameters for one mass building model according to given parameters of a heat pump.
@@ -205,12 +205,12 @@ class CalcParameters:
         :param t_a_design: design nominal outdoor temperature in °C
         :param t_a: outdoor temperature in test point
         :param q_design: nominal heating power in W
+        :param c_design: design thermal capacity in J/K/W_design
         :param PLC: rel heating load in test point (0...1)
         :param t_flow_design: nominal design flow temperature in °C
         :param t_flow_plc: flow temperature in test point in °C
         :param t_b: nominal building temperature (standard value: 20 °C) in °C
         :param m_dot_H_design: design mass flow of heating system in kg/s
-        :param tau_h: time constant of heating system in design point in s
         :param boostHeat: if true use virtual heater
         :param maxPowBooHea: maximal power of virtual heater in W
         :param hydraulicSwitch: if true use hydraulic switch
@@ -231,7 +231,7 @@ class CalcParameters:
         self.PLC = PLC
         self.t_flow_design = t_flow_design
         self.t_flow_plc = t_flow_plc
-        self.tau_h = tau_h
+        self.c_design = c_design
         self.hydraulicSwitch = hydraulicSwitch
         self.boostHeat = boostHeat
         self.maxPowBooHea = maxPowBooHea
@@ -289,7 +289,10 @@ class CalcParameters:
         self.t_start_h = self.t_flow_plc - self.delta_T_cond
 
         # Thermal capacity of heating system
-        self.mcp_h = self.tau_h * self.ua_hb_design
+        # self.mcp_h = self.tau_h * self.ua_hb_design
+        self.mcp_h = self.c_design * self.q_design
+        self.tau_h = self.mcp_h / self.ua_hb
+        # print("tau_h: ", self.tau_h)
 
     def update(self):
         """Update this class after changing one of the init parameters."""
@@ -301,7 +304,7 @@ class CalcParameters:
             t_flow_design=self.t_flow_design,
             t_flow_plc=self.t_flow_plc,
             m_dot_H_design=self.m_dot_H_design,
-            tau_h=self.tau_h,
+            c_design=self.c_design,
             t_b=self.t_b,
             boostHeat=self.boostHeat,
             maxPowBooHea=self.maxPowBooHea,
