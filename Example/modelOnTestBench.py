b61989419ebbf9e423193360bc298605e5c5323b
Jonas Klauke <jonas.klauke@eonerc.rwth-aachen.de>
2026-09-07 10:32:46 +0200

    Splitted saving of results and settings; settings are now saved at the beginning; added date and time to file name for export; added try-except-finally block for keyboard interrupt

--- Example/modelOnTestBench.py
+++ Example/modelOnTestBench.py
@@ -20,29 +20,17 @@ import warnings
 - You can stop the calculations by pressing ctrl + h + p at the same time. After that all results and settings will be 
   saved.
 - If the above keyboard shortcuts do not seem to work, hold all keys for one or two seconds.
+- This script shows an example on how to save the results. This could also be done differently.
 """
 
-def save_results_and_settings(final):
-    """Save results and settings to Excel files.
 
-    :param final: if True, save final results; if False, save intermediate results.
+def save_settings(version):
+    """Save settings to Excel files.
+
+    :param version: string to indicate the version of the settings.
     """
-    # Save results
-    if not os.path.exists(export_path):
-        os.makedirs(export_path)
 
-    if final:
-        res_path = export_path + f"\\results_final_{test_name}.xlsx"
-        set_path = export_path + f"\\settings_final_{test_name}.xlsx"
-    else:
-        res_path = export_path + f"\\results_v{count_intermediate_results}_{test_name}.xlsx"
-        set_path = export_path + f"\\settings_v{count_intermediate_results}_{test_name}.xlsx"
-
-    print(f"Saving results (path={res_path})")
-    res_tb_df = pd.DataFrame(res_tb)
-    res_bui_df = pd.DataFrame(res_bui)
-    results_df = pd.concat([res_tb_df, res_bui_df], axis=1)
-    results_df.to_excel(res_path)
+    set_path = export_path + f"\\settings_v{version}_{test_name}.xlsx"
 
     # Save settings
     print(f"Saving settings (path={set_path})")
@@ -50,23 +38,43 @@ def save_results_and_settings(final):
         "test_name": test_name,
         "export_path": export_path,
         "stepSize": stepSize,
-        "time_start": time_start,
-        "time_end": time_end,
-        "current time": datetime.datetime.now(),
+        "heating_plc": heating_plc,
+        "time_start": str(time_start),
+        "time_end": str(time_end),
+        "current time": str(datetime.datetime.now()),
     }
     set_dict.update({"info": "the following settings are from the one mass building"})
     set_dict.update(BamBuilding.__dict__)
+    set_dict.update({"info1": "the following settings are from the thermal mass (MassH)"})
+    set_dict.update(BamBuilding.MassH.__dict__)
     settings_df = pd.DataFrame.from_dict(set_dict, orient='index')
     settings_df.to_excel(set_path)
+
+
+def save_results(version):
+    """Save results to Excel files.
+
+    :param version: string to indicate the version of the settings.
+    """
+
+    res_path = export_path + f"\\results_v{version}_{test_name}.xlsx"
+
+    # Save results
+    print(f"Saving results (path={res_path})")
+    res_tb_df = pd.DataFrame(res_tb)
+    res_bui_df = pd.DataFrame(res_bui)
+    results_df = pd.concat([res_tb_df, res_bui_df], axis=1)
+    results_df.to_excel(res_path)
 
 
 if __name__ == "__main__":
     time_start = datetime.datetime.now()
     time_end = None
+    time_start_name = time_start.strftime("%Y_%m_%d_%H_%M_%S")
 
     # --- START OF USER INPUT ---
     # Name of this test: do not use spaces or special characters
-    test_name = "test"      # TODO Insert a name for the test here
+    test_name = f"test_{time_start_name}"   # TODO Insert a name for the test here. The additional {time_start_name} is used to avoid overwriting existing files. You could also remove this on your own risk.
 
     # Export path
     export_path = "."       # TODO Insert an export path here, where the results will be stored
@@ -85,6 +93,13 @@ if __name__ == "__main__":
 
     # --- END OF USER INPUT ---
 
+    # Create save path
+    if not os.path.exists(export_path):
+        os.makedirs(export_path)
+
+    # Save the settings
+    save_settings(version="Start")
+
     # Lists to store results
     res_tb = []                     # measurements from test bench
     res_bui = []                    # data from one mass model
@@ -93,87 +108,94 @@ if __name__ == "__main__":
     # --- Start of measurements and calculations ---
     print("\nStarting measurements at test bench and calculations of one mass model")
     print("Press ctrl + h + p to stop measurements and calculations (results will be saved afterwards)")
-    while True:
-        t1 = time.time()
+    try:
+        while True:
+            t1 = time.time()
 
-        # Get current measurements from test bench
-        t_sup_test_bench = 52       # in °C;    TODO Connect your test bench here!
-        m_w_hp = 720/3600           # in kg/s;  TODO Connect your test bench here!
-        t_ret_test_bench = 45       # in °C;    TODO Connect your test bench here!
+            # Get current measurements from test bench
+            t_sup_test_bench = 52       # in °C;    TODO Connect your test bench here!
+            m_w_hp = 720/3600           # in kg/s;  TODO Connect your test bench here!
+            t_ret_test_bench = 45       # in °C;    TODO Connect your test bench here!
 
-        if heating_plc:
-            if t_sup_test_bench - t_ret_test_bench < 0.2:
-                # heating_test_bench is a boolean value: True if heating (or cooling), False if defrosting
-                heating_test_bench = False      # If you do not want to have zero load during defrosting, just change this variable to "True"
+            if heating_plc:
+                if t_sup_test_bench - t_ret_test_bench < 0.2:
+                    # heating_test_bench is a boolean value: True if heating (or cooling), False if defrosting
+                    heating_test_bench = False      # If you do not want to have zero load during defrosting, just change this variable to "True"
+                else:
+                    heating_test_bench = True
             else:
                 heating_test_bench = True
-        else:
-            heating_test_bench = True
 
-        # Save current state of test bench (tb) and one mass building (bui)
-        res_tb.append(
-            {
-                "time": str(datetime.datetime.now()),
-                "t_sup_test_bench": t_sup_test_bench,
-                "t_ret_test_bench": t_ret_test_bench,
-                "m_w_hp": m_w_hp,
-                "heating_test_bench": heating_test_bench
-            }
-        )
-        res_bui.append(
-            {
-                "T_h": BamBuilding.MassH.T,
-                "q_dot_hp": BamBuilding.q_dot_hp,
-                "q_dot_hb": BamBuilding.q_dot_hb,
-                "m_dot_H_design": BamBuilding.m_flow_design,
-                "T_set_supply": BamBuilding.t_flow_design,
-                "T_bui": BamBuilding.t_b_design + 273.15,
-            }
-        )
+            # Save current state of test bench (tb) and one mass building (bui)
+            res_tb.append(
+                {
+                    "time": str(datetime.datetime.now()),
+                    "t_sup_test_bench": t_sup_test_bench,
+                    "t_ret_test_bench": t_ret_test_bench,
+                    "m_w_hp": m_w_hp,
+                    "heating_test_bench": heating_test_bench
+                }
+            )
+            res_bui.append(
+                {
+                    "T_h": BamBuilding.MassH.T,
+                    "q_dot_hp": BamBuilding.q_dot_hp,
+                    "q_dot_hb": BamBuilding.q_dot_hb,
+                    "m_dot_H_design": BamBuilding.m_flow_design,
+                    "T_set_supply": BamBuilding.t_flow_design,
+                    "T_bui": BamBuilding.t_b_design,
+                }
+            )
 
-        # Stop measurements and calculations with keyboard
-        if (keyboard.is_pressed("ctrl") and keyboard.is_pressed("h") and keyboard.is_pressed("p") or
-                (keyboard.is_pressed("ctrl") and keyboard.is_pressed("c"))):
-            time_end = datetime.datetime.now()
-            print("\nMeasurements and calculations were stopped by keyboard interrupt!\n")
-            break
+            # Stop measurements and calculations with keyboard
+            if (keyboard.is_pressed("ctrl") and keyboard.is_pressed("h") and keyboard.is_pressed("p") or
+                    (keyboard.is_pressed("ctrl") and keyboard.is_pressed("c"))):
+                time_end = datetime.datetime.now()
+                print("\nMeasurements and calculations were stopped by keyboard interrupt!\n")
+                break
 
-        # Save intermediate results
-        if keyboard.is_pressed("ctrl") and keyboard.is_pressed("s"):
-            print("\nSaving intermediate results!\n")
-            save_results_and_settings(final=False)
-            count_intermediate_results += 1
+            # Save intermediate results
+            if keyboard.is_pressed("ctrl") and keyboard.is_pressed("s"):
+                print("\nSaving intermediate results!\n")
+                save_results(version=str(count_intermediate_results))
+                save_settings(version=str(count_intermediate_results))
+                count_intermediate_results += 1
 
-        # Calculate time step in Building model
-        BamBuilding.doStep(
-            t_sup=t_sup_test_bench,
-            t_ret_mea=t_ret_test_bench,
-            m_w_hp=m_w_hp,
-            stepSize=stepSize,
-            heating=heating_test_bench
-        )
+            # Calculate time step in Building model
+            BamBuilding.doStep(
+                t_sup=t_sup_test_bench,
+                t_ret_mea=t_ret_test_bench,
+                m_w_hp=m_w_hp,
+                stepSize=stepSize,
+                heating=heating_test_bench
+            )
 
-        # Set the return temperature from one mass model (BamBuilding.t_ret) at the test bench
-        # TODO Connect your test bench here! After that you can comment the following print
-        print("Has to be connected to test bench! Set return temperature for test bench: " + str(BamBuilding.t_ret))
+            # Set the return temperature from one mass model (BamBuilding.t_ret) at the test bench
+            # TODO Connect your test bench here! After that you can comment the following print
+            print("Has to be connected to test bench! Set return temperature for test bench: " + str(BamBuilding.t_ret))
 
-        # Print current state of test bench and one mass model
-        print("TEST BENCH: Supply Temperature: " + str(round(t_sup_test_bench, 2)) + " °C;" +
-              " m_flow : " + str(round(m_w_hp, 2)) + " kg/s;" +
-              " Return Temperature: " + str(round(BamBuilding.t_ret, 2)) + " °C; " +
-              "ONE MASS MODEL: T_H = " + str(round(BamBuilding.MassH.T, 2)) + " °C;" +
-              " q_flow_hp = " + str(round(BamBuilding.q_dot_hp, 2)) + " W;" +
-              " q_flow_hb = " + str(round(BamBuilding.q_dot_hb, 2)) + " W")
+            # Print current state of test bench and one mass model
+            print("TEST BENCH: Supply Temperature: " + str(round(t_sup_test_bench, 2)) + " °C;" +
+                  " m_flow : " + str(round(m_w_hp, 2)) + " kg/s;" +
+                  " Return Temperature: " + str(round(BamBuilding.t_ret, 2)) + " °C; " +
+                  "ONE MASS MODEL: T_H = " + str(round(BamBuilding.MassH.T, 2)) + " °C;" +
+                  " q_flow_hp = " + str(round(BamBuilding.q_dot_hp, 2)) + " W;" +
+                  " q_flow_hb = " + str(round(BamBuilding.q_dot_hb, 2)) + " W")
 
-        # Sleep to run in real time
-        t2 = time.time()
-        sleepTime = stepSize-(t2-t1)
-        if sleepTime > 0:
-            print('Sleep time = ' + str(sleepTime) + ' s\n')
-            time.sleep(sleepTime)
-        else:
-            warnings.warn('Warning: Loop too slow!\n')
-            sleepTime = 0
+            # Sleep to run in real time
+            t2 = time.time()
+            sleepTime = stepSize-(t2-t1)
+            if sleepTime > 0:
+                print('Sleep time = ' + str(sleepTime) + ' s\n')
+                time.sleep(sleepTime)
+            else:
+                warnings.warn('Warning: Loop too slow!\n')
+                sleepTime = 0
 
-    # Save final results and settings
-    save_results_and_settings(final=True)
+    except KeyboardInterrupt:
+        print("Script was interrupted by the user (keyboard interrupt)!")
+
+    finally:
+        # Save final results and settings
+        save_results(version="Final")
+        save_settings(version="Final")
